d3c69453bcafcd53702252c5adc4426ce3cca6ed
Aadil151 <ahmadaadil084@gmail.com>
2022-12-26 14:19:12 +0000

    connected login page to aws using amplify

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const config = {
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_LBxaX2WMX",
+      USER_POOL_ID: "us-east-1_LBxaXZWMX",
       APP_CLIENT_ID: "hmci5ddrgio99igf39j2hqutr",
       IDENTITY_POOL_ID: "us-east-1:5e30bf1a-e4b2-4256-a130-fff78e90b0f4",
     },
