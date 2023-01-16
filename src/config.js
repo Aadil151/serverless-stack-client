6a9bc90fe235bc5fea1ac0f7a5dac1ebd3cd8378
Aadil151 <ahmadaadil084@gmail.com>
2023-01-16 22:12:16 +0000

    created new notes and connected it to API

--- src/config.js
+++ src/config.js
@@ -13,6 +13,8 @@ const config = {
       APP_CLIENT_ID: "hmci5ddrgio99igf39j2hqutr",
       IDENTITY_POOL_ID: "us-east-1:5e30bf1a-e4b2-4256-a130-fff78e90b0f4",
     },
+    MAX_ATTACHMENT_SIZE: 5000000,
   };
   
   export default config;
+  
